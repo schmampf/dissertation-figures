d13a312ea87bfda601656d50a65ba3dab9b46eaa
schmampf <oliver.irtenkauf@uni-konstanz.de>
2026-07-16 10:36:06 +0200

    pre calculate grid

---
+++ atomic_contact/grid.py
@@ -0,0 +1,14 @@
+# prepare database
+import numpy as np
+from mar_fit import MARGrid, prepare_mar_database
+
+grid = MARGrid(
+    V_mV=np.linspace(-0.9, 0.9, 1801),
+    tau=np.linspace(0, 1, 1001),
+    T_K=0.0,
+    Delta_meV=np.linspace(0.187, 0.191, 9),
+    gamma_meV=1e-6,
+    # sigmaV_mV=np.linspace(0, 0.05, 51),
+    sigmaV_mV=0,
+)
+database = prepare_mar_database(grid)
